0249c3f081c5d6e7b974736ac16118a4267dfcab
Aditya Savale <adityasavale1@gmail.com>
2025-12-11 10:34:04 +0530

    calculation

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+
+def addition(a,b):
+    return a+b
+
+def substraction(a,b):
+    return a-b
+
+a= int(input("enter first"))
+b= int(input("enter second"))
+
+print(f"the addtion is {addition(a,b)}")
+print(f"the addtion is {substraction(a,b)}")
